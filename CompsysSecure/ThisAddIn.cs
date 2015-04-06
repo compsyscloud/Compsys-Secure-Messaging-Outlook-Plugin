5c6adaab627f6dd8e877f599e2b6e9ebec762f81
Jacob Dixon <jdixon@knowmoreit.com>
2015-04-06 11:33:51 -0500

    Updates

--- CompsysSecure/ThisAddIn.cs
+++ CompsysSecure/ThisAddIn.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using Outlook = Microsoft.Office.Interop.Outlook;
-using Office = Microsoft.Office.Core;
-
+﻿
 namespace CompsysSecure
 {
     public partial class ThisAddIn
